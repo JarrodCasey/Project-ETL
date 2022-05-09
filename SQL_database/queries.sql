45217cc476500f656d253be0cf65a023086614a5
Ash <ash.taowq@gmail.com>
2022-05-09 18:22:17 +1000

    updated queries.sql with comments

--- SQL_database/queries.sql
+++ SQL_database/queries.sql
@@ -4,13 +4,15 @@ select movie_list.movie_id, movie_list.movie_title, movie_list.release_year,
        join rating_top100
        on movie_list.movie_id = rating_top100.movie_id;
 
+-- 1. Calculate the average rating per movie based on the mubi_ratings_data.csv dataset.
 select m.movie_title, r.avg_rating
   from movie_list as m
   join rating_top100 as r
     on m.movie_id= r.movie_id
 	order by r.avg_rating desc
 	limit 5;
-	
+
+-- 2. Calculate the number of ratings received per movie (this gives an indication of how many times a movie is watched).
 select m.movie_title, r.count
   from movie_list as m
   join rating_top100 as r
@@ -18,10 +20,11 @@ select m.movie_title, r.count
 	order by r.count desc
 	limit 5;
 
-select m.release_year,round (cast(avg(avg_rating) as numeric),2) asavg_rating
+-- 3. Years with the highest average rating for the release movie 
+select m.release_year,round (cast(avg(avg_rating) as numeric),2) as avg_rating
   from movie_list as m
   join rating_top100 as r
     on m.movie_id= r.movie_id
 	group by m.release_year
-	order by asavg_rating desc
+	order by avg_rating desc
 	limit 5;
