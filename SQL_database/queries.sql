4486d97d8745aa3a3b855eb3f2953665841f20b4
Ash <ash.taowq@gmail.com>
2022-05-09 18:18:45 +1000

    updated queries.sql

--- SQL_database/queries.sql
+++ SQL_database/queries.sql
@@ -3,3 +3,25 @@ select movie_list.movie_id, movie_list.movie_title, movie_list.release_year,
        from movie_list
        join rating_top100
        on movie_list.movie_id = rating_top100.movie_id;
+
+select m.movie_title, r.avg_rating
+  from movie_list as m
+  join rating_top100 as r
+    on m.movie_id= r.movie_id
+	order by r.avg_rating desc
+	limit 5;
+	
+select m.movie_title, r.count
+  from movie_list as m
+  join rating_top100 as r
+    on m.movie_id= r.movie_id
+	order by r.count desc
+	limit 5;
+
+select m.release_year,round (cast(avg(avg_rating) as numeric),2) asavg_rating
+  from movie_list as m
+  join rating_top100 as r
+    on m.movie_id= r.movie_id
+	group by m.release_year
+	order by asavg_rating desc
+	limit 5;
